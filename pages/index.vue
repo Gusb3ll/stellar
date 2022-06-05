9ab0c76699f2f9aac9c46892bf85f4032192ea55
Gusb3ll <Kitpipat.0647@gmail.com>
2022-06-05 13:28:44 +0700

    feat: projects section

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,49 @@
 <script setup>
+import { useWindowScroll } from '@vueuse/core'
 import GlitchedWriter from 'vue-glitched-writer'
+import projects from '~/assets/projects.json'
 
 const writer = GlitchedWriter
 
-const text = ['Gusbell', 'Kitpipat Jaritwong']
-const loading = ref(true)
+const { y } = useWindowScroll()
 
-onMounted(() => {
-  setTimeout(() => {
-    loading.value = false
-  }, 500)
-})
+const projActive = ref(false)
 </script>
 
 <template>
-  <div class="flex flex-col h-full">
-    <section id="header" class="flex min-h-screen justify-center">
-      <Transition name="fade">
-        <div v-if="!loading" class="flex flex-col justify-center items-center gap-8">
-          <h1 class="md:text-7xl text-4xl text-white tracking-wide uppercase select-none">
-            <writer :text="text" preset="nier" :queue="{ loop: true, interval: 5000 }" />
-          </h1>
-          <AppSocial />
-        </div>
-      </Transition>
+  <main class="flex flex-col h-full overflow-hidden">
+    <section id="title" class="flex min-h-screen justify-center">
+      <div class="flex flex-col justify-center items-center gap-8">
+        <h1 class="md:text-7xl text-4xl text-white text-center tracking-wide uppercase select-none">
+          <writer :text="['Gusbell', 'Kitpipat Jaritwong']" preset="nier" :queue="{ loop: true, interval: 5000 }" appear />
+        </h1>
+        <AppSocial />
+      </div>
     </section>
-  </div>
+    <section id="projects" ref="projectSection" class="flex min-h-screen w-screen justify-center">
+      <div class="flex flex-col gap-8 mt-8">
+        <Transition name="textFade">
+          <h1 v-if="y > 250 || projActive" class="md:text-5xl text-3xl text-white text-center tracking-wide uppercase select-none">
+            <writer text="Projects" preset="nier" appear @start="projActive = !projActive" />
+          </h1>
+        </Transition>
+        <Transition name="fade">
+          <div v-show="projActive" class="grid h-full w-full xl:grid-cols-3 md:grid-cols-2 grid-cols-1 grid-flow-row place-items-center gap-8">
+            <AppProject
+              v-for="({ title, description, githubEnable, websiteEnable, githubUrl, websiteUrl }, index) in projects"
+              :key="index"
+              :title="title"
+              :description="description"
+              :github-enable="githubEnable"
+              :website-enable="websiteEnable"
+              :github-url="githubUrl"
+              :website-url="websiteUrl"
+            />
+          </div>
+        </Transition>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped>
@@ -35,6 +53,15 @@ onMounted(() => {
 }
 .fade-enter-from,
 .fade-leave-to {
+  opacity: 0;
+}
+
+.textFade-enter-active,
+.textFade-leave-active {
+  transition: opacity 0.5s;
+}
+.textFade-enter-from,
+.textFade-leave-to {
   opacity: 0;
 }
 </style>
