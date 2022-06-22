711597b32da548778696c8a784c241ce7a827c5e
Gusb3ll <Kitpipat.0647@gmail.com>
2022-06-23 00:57:42 +0700

    fix: hydration issue

--- components/app/layout/nav.vue
+++ components/app/layout/nav.vue
@@ -1,11 +1,16 @@
 <script setup>
 const navItems = [{ name: 'Home', url: '/' }, { name: 'Profile', url: '/profile' }, { name: 'Projects', url: '/projects' }, { name: 'Designs', url: '/designs' }, { name: 'Works', url: '/works' }]
+const loading = ref(true)
+
+onMounted(() => {
+  loading.value = false
+})
 </script>
 
 <template>
   <div class="z-50 relative flex h-22 w-screen justify-center items-center">
-    <Transition name="nav">
-      <div class="sm:space-x-8 space-x-4">
+    <Transition name="fadeFast">
+      <div v-if="!loading" class="sm:space-x-8 space-x-4">
         <NuxtLink v-for="({ name, url }, index) in navItems" :key="index" :to="url" class="sm:text-xl text-lg text-white font-light hover:opacity-70 transition-all">
           {{ name }}
         </NuxtLink>
@@ -13,12 +18,3 @@ const navItems = [{ name: 'Home', url: '/' }, { name: 'Profile', url: '/profile'
     </Transition>
   </div>
 </template>
-
-<style scoped>
-.nav-enter-active, .nav-leave-active {
-  transition: opacity 0.45s ease, transform 0.45s ease;
-}
-.nav-enter-from, .nav-leave-to  {
-  opacity: 0; transform: translateY(35px);
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,22 +2,29 @@
 import GlitchedWriter from 'vue-glitched-writer'
 
 const writer = GlitchedWriter
+const loading = ref(true)
 
 definePageMeta({
   layout: 'default',
+})
+
+onMounted(() => {
+  loading.value = false
 })
 </script>
 
 <template>
   <div class="flex min-h-screen justify-center -mt-22">
-    <div class="flex flex-col justify-center items-center gap-8">
-      <h1 class="md:text-7xl text-3xl text-white text-center tracking-wide uppercase select-none">
-        <writer :text="['Gusbell', 'Kitpipat Jaritwong']" preset="nier" :queue="{ loop: true, interval: 4000 }" appear />
-      </h1>
-      <AppSocial />
-      <NuxtLink to="https://webring.wonderful.software#gusbell.tech" title="วงแหวนเว็บ" target="_blank">
-        <img alt="วงแหวนเว็บ" width="32" height="32" src="https://webring.wonderful.software/webring.white.svg">
-      </NuxtLink>
-    </div>
+    <Transition name="fadeFast">
+      <div v-if="!loading" class="flex flex-col justify-center items-center gap-8">
+        <h1 class="md:text-7xl text-3xl text-white text-center tracking-wide uppercase select-none">
+          <writer :text="['Gusbell', 'Kitpipat Jaritwong']" preset="nier" :queue="{ loop: true, interval: 4000 }" appear />
+        </h1>
+        <AppSocial />
+        <NuxtLink to="https://webring.wonderful.software#gusbell.tech" title="วงแหวนเว็บ" target="_blank">
+          <img alt="วงแหวนเว็บ" width="32" height="32" src="https://webring.wonderful.software/webring.white.svg">
+        </NuxtLink>
+      </div>
+    </Transition>
   </div>
 </template>
