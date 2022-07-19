7dd8f9004e2be1fe358f9915dd553b11d66c2009
Gusb3ll <Kitpipat.0647@gmail.com>
2022-07-19 21:52:47 +0700

    feat: add creatorsgarten

--- pages/index.vue
+++ pages/index.vue
@@ -16,9 +16,14 @@ definePageMeta({
           <writer :text="['Gusbell', 'Kitpipat Jaritwong']" preset="nier" :queue="{ loop: true, interval: 4000 }" appear />
         </h1>
         <AppSocial />
-        <NuxtLink to="https://webring.wonderful.software#gusbell.tech" title="วงแหวนเว็บ" target="_blank">
-          <img alt="วงแหวนเว็บ" width="32" height="32" src="https://webring.wonderful.software/webring.white.svg">
-        </NuxtLink>
+        <div class="flex flex-row gap-8">
+          <NuxtLink to="https://webring.wonderful.software#gusbell.tech" title="วงแหวนเว็บ" target="_blank">
+            <img alt="วงแหวนเว็บ" width="32" height="32" src="https://webring.wonderful.software/webring.white.svg">
+          </NuxtLink>
+          <NuxtLink to="https://creatorsgarten.org/ring" title="creatorsgarten" target="_blank">
+            <img src="~/assets/logo/creator.svg" width="32" height="32" alt="creatorsgarten" class="text-white">
+          </NuxtLink>
+        </div>
       </div>
     </Transition>
   </div>
